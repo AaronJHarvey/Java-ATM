ac5023b3786f9fe35ba2c7cd704f4c95874ac97b
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-11 10:33:06 -0500

    Creates general login prompt

--- src/ATM.java
+++ src/ATM.java
@@ -1,15 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+
+import java.util.Scanner;
+
 
 public class ATM {
 
-    static User a = new User("Aaron");
-    static ArrayList<User> users= new ArrayList<User>(Arrays.asList(a));
-        
+   
     public static void main(String[] args) {
-      
-        }
+        login();
     }
+
+
+     public static void login(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hello! Please enter your UserID: ");
+        int inputId = in.nextInt();
+        System.out.print(inputId);
+    }
+        
+}
+    
 
 
