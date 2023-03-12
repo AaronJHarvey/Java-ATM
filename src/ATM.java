cb09b3fd0c61a31c845963a18667a599ff52103e
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-12 13:26:32 -0400

    .

--- src/ATM.java
+++ src/ATM.java
@@ -1,21 +1,39 @@
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class ATM {
-
+    private static User aaron = new User("Aaron", 1234);
+    private static User alex = new User("Alex", 5678);
+    private static ArrayList<User> users = new ArrayList<User>(
+        Arrays.asList(aaron, alex)
+    ) ;
+    
+    // public static void login() {
+    //     Scanner in = new Scanner(System.in);
+    //     System.out.print("Hello! Please enter your UserID: ");
+    //    int inputId = in.nextInt();
+    //     System.out.print(inputId);
+    // }a
+    public static void login() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hello! Please enter your UserID: ");
+        String userInput = in.nextLine().toLowerCase();
+        System.out.print("\n\n" + users.toString().contains(userInput)+ "\n\n");
+        System.out.print("\n\n" + userInput.getClass() + "\n\n");
+    //    System.out.print("\n\n" + users.contains(userInput) + "\n\n");
+    }
    
     public static void main(String[] args) {
+
         login();
     }
 
 
-     public static void login(){
-        Scanner in = new Scanner(System.in);
-        System.out.print("Hello! Please enter your UserID: ");
-        int inputId = in.nextInt();
-        System.out.print(inputId);
-    }
+  
         
 }
     
--- src/User.java
+++ src/User.java
@@ -1,20 +1,26 @@
-
+import java.util.ArrayList;
 
 public class User {
     private String name;
     private int password;
-    private String accounts;
+    // private ArrayList<Account> accounts;
 
-    public User(String name) {
+    public User(String name, int password) {
         this.name = name;
+        this.password = password;
     }
 
     public void changeName(String name){
         this.name = name;
     }
 
+    public void getPassword() {
+        System.out.print("\n\n" + String.valueOf(password) + "\n\n");
+    }
 
     public String toString() {
         return String.valueOf(name);
     }
+
+    
 }
