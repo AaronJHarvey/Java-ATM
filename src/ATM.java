9df9466ff5f108ce9b1a80c096958cf72ddf592d
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-13 12:43:21 -0400

    Asks user for name input If it is found in the users list that is made the currentUser

--- src/ATM.java
+++ src/ATM.java
@@ -20,7 +20,6 @@ public class ATM {
     System.out.print("Hello! Please enter your name: ");
     String inputName = in.nextLine();
 
-
     for (int i = 0; i < users.size(); i++) {
       if (users.get(i).toString().toLowerCase().equals(inputName.toLowerCase())) {
         currentUser = users.get(i);
@@ -29,31 +28,12 @@ public class ATM {
     }
   }
   
-    
-    // public static void login() {
-    //     Scanner in = new Scanner(System.in);
-    //     System.out.print("Hello! Please enter your UserID: ");
-    //    int inputId = in.nextInt();
-    //     System.out.print(inputId);
-    // }a users.add(a);
-      // users.add(al);
-      
-      //   Scanner in = new Scanner(System.in);
-      //   System.out.println("Welcome! Please put in your Name: ");
-      //   String userInput = in.nextLine().toLowerCase();
-
-
-      //   for (int i = 0; i < users.size(); i++) {
-      //     //  System.out.print( users.get(i).getName().toLowerCase().getClass() == userInput.toString().getClass() );
-      //     if (users.get(i).getName().toLowerCase().equals(userInput)) {
-      //        currentUser = users.get(i);
-      //       break;
-      //     };
-
-   
-    public static void main(String[] args) {
-      login();
-    }    
+  
+  
+  
+  public static void main(String[] args) {
+    login();
+  }    
 }
     
 
