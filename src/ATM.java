ec3d355957c359aba8ef5a349728deac7a1003ba
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-15 12:18:39 -0400

    Moves login to Session class.

--- src/ATM.java
+++ src/ATM.java
@@ -6,39 +6,15 @@ import java.util.Scanner;
 
 
 public class ATM {
-  private static ArrayList<User> users = new ArrayList<User>();
-  static User a = new User("Aaron", 1234);
-  static User al = new User("Alex", 5678);
-  static User currentUser;
+  static User currentUser ;
 
-  public static void login() {
-    users.add(a);
-    users.add(al);
-
-
-    Scanner in = new Scanner(System.in);
-    do {
-      System.out.println("Hello! Please enter your name: ");
-      String inputName = in.nextLine();
-
-      for (int i = 0; i < users.size(); i++) {
-        if (users.get(i).toString().toLowerCase().equals(inputName.toLowerCase())) {
-          currentUser = users.get(i);
-          break;
-        }
-      }
-      if (currentUser == null) {
-        System.out.print("User Not Found. Please Try Again! \n");
-      }
-    } while (currentUser == null);
-  }
-  
-  
-  
   public static void main(String[] args) {
-    login();
-    System.out.print(currentUser);
-  }    
+    
+    Session.login();
+    currentUser = Session.currentUser;
+
+
+  }
 }
     
 
---
+++ src/Session.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Session {
+
+    private static ArrayList<User> users = new ArrayList<User>();
+    static User a = new User("Aaron", 1234);
+    static User al = new User("Alex", 5678);
+    public static User currentUser;
+
+    public static void login() {
+        users.add(a);
+        users.add(al);
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.println("Hello! Please enter your name: ");
+            String inputName = in.nextLine();
+
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).toString().toLowerCase().equals(inputName.toLowerCase())) {
+                    currentUser = users.get(i);
+                    break;
+                }
+            }
+            if (currentUser == null) {
+                System.out.print("User Not Found. Please Try Again! \n");
+            }
+        } while (currentUser == null);
+    }
+}
