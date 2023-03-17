1980431477f57464df70d8152846092392bd839f
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-17 16:02:36 -0400

    Implements Switch Case for chooseTransaction()

--- src/Account.java
+++ src/Account.java
@@ -16,17 +16,43 @@ public class Account {
 
     }
 
-    public void deposit(int amount) {
-        accountBalance += amount;
-        System.out.print(accountBalance);
+    public void deposit() {
+        do {
+
+        }
+        // accountBalance += amount;
+        // System.out.print(accountBalance);
     }
 
     public void chooseTransaction() {
-        System.out.print("\n\n Please choose what you wish to do: \n");
+        System.out.print("\n\n Please choose what you wish to do: \n" +
+         " Press 1 for Deposit \n" + 
+         " Press 2 for Withdraw \n" + 
+         " Press 3 for Current Balance \n"+
+         " Press 4 to Exit \n"
+        );
         Scanner in = new Scanner(System.in);
         int transaction = in.nextInt();
+        switch (transaction) {
+            case 1:
+                deposit();
+                break;
+            case 2:
+                System.out.print("Withdraw");
+                // withdraw();
+                break;
+            case 3:
+                System.out.print("Balance");
+                // getBalance();
+                break;
+            case 4:
+                break;
+            default:
+                System.out.print("Invalid entry. Please Try Again");
+        }
+
         if (transaction == 1) {
-            deposit(2);
+            deposit();
         }
     }
 
